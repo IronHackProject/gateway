6f58ec98a0a361f59640f583826cab1ae9137de6
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-26 12:57:08 +0200

    configure route gateway

---
+++ src/main/java/com/gateway/gateway/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.gateway.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("userService", r -> r.path("/api/user/**")
+                        .uri("lb://userService"))
+                .build();
+    }
+}
